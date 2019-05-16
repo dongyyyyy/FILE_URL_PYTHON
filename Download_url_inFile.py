0f1e5209002b6f2b2be4b1fb5944947a2e84e8e7
dongyyyyy <ehddmleheheh@naver.com>
2019-05-16 17:16:31 +0900

    update

--- Download_url_inFile.py
+++ Download_url_inFile.py
@@ -1,7 +1,19 @@
 import urllib.request
 import re
+directory = []
+print(directory)
 
 p = re.compile('\S+')
+f1 = open ("./checkDirectory.txt","r")
+while True:
+    line = f1.readline()
+    if not line: break  # 파일 끝
+    if line[0:1] == '-':
+        directory.append(line[1:].rstrip('\n'))
+    else :
+        directory.append(line.rstrip('\n'))
+f1.close()
+print(directory);
 f = open("./fall11_urls.txt","rt",encoding='UTF8') # 읽기 모드로 오픈
 strs = "in_person"
 count = 1
@@ -9,9 +21,9 @@ while True: # 무한 반복
     line = f.readline()
     if not line : break # 파일 끝
     url = p.findall(line) #  url변수에 파일 내용 저장
-    if ( url[0][0:3] == 'n10' or url[0][0:3]=='n095' or url[0][0:3]=='n096' or url[0][0:3]=='n097' or url[0][0:3]=='n098' or url[0][0:3]=='n099'):
+    if url[0][0:9] in directory :
         try:
-            print(url[0]);
+            print(url[0][0:9]);
             urllib.request.urlretrieve(url[1], "G:/imagenet/" + strs + str(count) + ".jpg")
             count += 1
         except:
