ba7b2a6e38fb8a9dace7885c6a0c6a28c8573298
dongyyyyy <ehddmleheheh@naver.com>
2019-05-17 11:00:55 +0900

    update

--- Download_url_inFile.py
+++ Download_url_inFile.py
@@ -14,6 +14,7 @@ def main():
         if line[0:1] == '-':
             line.rstrip('\n')
             line = line.replace('-','',1)
+            print(line)
             sub(line,count)
         else:
             line.rstrip('\n')
@@ -29,18 +30,22 @@ def sub(wnid,count):
         if line[0:1] == '-':
             line.rstrip('\n')
             line = line.replace('-', '', 1)
-            sub(line)
+            print(line)
+            sub(line,count)
         else:
             line.rstrip('\n')
             count = download(line,count)
 
 def download(wnid,count):
+    count = int(count)
     strs = "in_person"
-    f = open("./fall11_urls.txt","rt",encoding='UTF8') # 읽기 모드로 오픈
+    f = open("./fall11_urls.txt","rt",encoding='utf-8') # 읽기 모드로 오픈
+    print("current count : " + str(count))
     while True:
         downloadurl = f.readline()
+        if not downloadurl: break  # 파일 끝
         url = p.findall(downloadurl)  # url변수에 파일 내용 저장
-        if url[0][0:9] in wnid:
+        if url[0][0:9] in wnid and url[0][0:9] != "n00007846":
             try:
                 print("directory : "  + url[0][0:9])
                 urllib.request.urlretrieve(url[1], "G:/imagenet/" + strs + str(count) + ".jpg")
@@ -48,6 +53,7 @@ def download(wnid,count):
             except:
                 pass
     f.close()
+    print("return count : " + str(count))
     return count
 main()
 
